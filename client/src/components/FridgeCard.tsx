44d5f4bcfdb9f0db883e5585833935ed4b4d4ca7
Brandon Roffis <brandon.roffis@ferguson.com>
2022-06-14 09:24:59 -0400

    lint fixes

--- client/src/components/FridgeCard.tsx
+++ client/src/components/FridgeCard.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { FridgeCardProps } from '../assets/types';
+import { FridgeCardProps } from "../assets/types";
 
-export const FridgeCard = ({ id, name, location, latestTemp }: FridgeCardProps ) => {
+export const FridgeCard = ({
+  id,
+  name,
+  location,
+  latestTemp,
+}: FridgeCardProps) => {
   return (
-    <Link to={`/fridge/${id}`}className="fridge-card">
+    <Link to={`/fridge/${id}`} className="fridge-card">
       <div className="fridge-card__header">
-        <p className="fridge-card__name">{ name }</p>
-        <p className="fridge-card__location">{ location }</p>
+        <p className="fridge-card__name">{name}</p>
+        <p className="fridge-card__location">{location}</p>
       </div>
-      { 
-        latestTemp?.temperature ? (
-          <div className="fridge-card__info">
-            <p className="fridge-card__date">{ latestTemp.logTime }:</p>
-            <p className="fridge-card__temp">{ latestTemp.temperature }&#xb0;</p>
-          </div>
-        ) : (
-          <p className="fridge-card__no-temp">No temperature logged</p>
-        )
-      }
+      {latestTemp?.temperature ? (
+        <div className="fridge-card__info">
+          <p className="fridge-card__date">{latestTemp.logTime}:</p>
+          <p className="fridge-card__temp">{latestTemp.temperature}&#xb0;</p>
+        </div>
+      ) : (
+        <p className="fridge-card__no-temp">No temperature logged</p>
+      )}
     </Link>
-  )
-}
-
+  );
+};
